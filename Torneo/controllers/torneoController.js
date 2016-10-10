2d788b1456109d7588b409c970c187d0889bed69
Pennicillina <badinelli@gmail.com>
2016-10-10 22:15:14 +0200

    Amministrazione - pagina e dati
    Inserimento pagina amministrazione

--- Torneo/controllers/torneoController.js
+++ Torneo/controllers/torneoController.js
@@ -3,24 +3,35 @@ angular.module("torneo")
 
     $scope.data = {
     	partite: [
-        { partita: 1, tavolo: 1, giocatore: "Matteo", punti: 23, conclusa: true },
-		{ partita: 1, tavolo: 1, giocatore: "Ivo", punti: 34, conclusa: true},
-		{ partita: 1, tavolo: 1, giocatore: "Ludo", punti: 18, conclusa: true},
-		{ partita: 1, tavolo: 1, giocatore: "Beppe", punti: 9, conclusa: true},
-		{ partita: 1, tavolo: 2, giocatore: "Matteo", punti: 56, conclusa: true },
-		{ partita: 1, tavolo: 2, giocatore: "Ivo", punti: 32, conclusa: true},
-		{ partita: 1, tavolo: 2, giocatore: "Ludo", punti: 1, conclusa: true},
-		{ partita: 1, tavolo: 2, giocatore: "Beppe", punti: 17, conclusa: true},
-		{ partita: 2, tavolo: 2, giocatore: "Matteo", punti: 6, conclusa: true },
+        { partita: 1, tavolo: 1, giocatore: "Matteo", punti: 23, conclusa: false },
+		{ partita: 1, tavolo: 1, giocatore: "Ivo", punti: 34, conclusa: false },
+		{ partita: 1, tavolo: 1, giocatore: "Ludo", punti: 18, conclusa: false },
+		{ partita: 1, tavolo: 1, giocatore: "Beppe", punti: 9, conclusa: false },
+		{ partita: 1, tavolo: 2, giocatore: "Matteo", punti: 56, conclusa: false },
+		{ partita: 1, tavolo: 2, giocatore: "Ivo", punti: 32, conclusa: false },
+		{ partita: 1, tavolo: 2, giocatore: "Ludo", punti: 1, conclusa: false },
+		{ partita: 1, tavolo: 2, giocatore: "Beppe", punti: 17, conclusa: true },
+		{ partita: 2, tavolo: 2, giocatore: "Matteo", punti: 6, conclusa: false },
 		{ partita: 2, tavolo: 2, giocatore: "Ivo", punti: 22, conclusa: true},
-		{ partita: 2, tavolo: 2, giocatore: "Ludo", punti: 71, conclusa: true},
+		{ partita: 2, tavolo: 2, giocatore: "Ludo", punti: 71, conclusa: false},
 		{ partita: 2, tavolo: 2, giocatore: "Beppe", punti: 57, conclusa: true}],
 
 		classifica: [
 		{ giocatore: "Matteo", punteggio: 0 },
 		{ giocatore: "Ivo", punteggio: 0 },
 		{ giocatore: "Ludo", punteggio: 0 },
-		{ giocatore: "Beppe", punteggio: 0 }]
-	}; 
+		{ giocatore: "Beppe", punteggio: 0 }],
+
+		match: 4,
+
+		tavoli: 4
+	};
+
+	function HeaderController($scope, $location) 
+	{ 
+    	$scope.isActive = function (viewLocation) { 
+        	return viewLocation === $location.path();
+    	};
+	} 
 
 });
