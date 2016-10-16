fc1ba395f334e7223223709585eb2fe47a50a63e
Pennicillina <badinelli@gmail.com>
2016-10-16 18:52:22 +0200

    Punteggio
    Generazione punteggio per classifica

--- Torneo/controllers/adminController.js
+++ Torneo/controllers/adminController.js
@@ -26,28 +26,35 @@ angular.module("torneo")
 		}
 
 		$scope.generateScore = function (gameNum) {
-			var game = [];
-			for (var i = 0; $scope.data.partite.length; i++) {
-				if ($scope.data.partite[i]["partita"] == gameNum) {
-					game.push($scope.data.partite[i]);
+			var tables = $scope.data.partite.filter(function (obj) {
+				return obj.partita == $scope.data.activeGame;
+			});
+
+			var game = tables.filter(function (obj) {
+				return obj.tavolo == '2';
+			});
+
+			game = game.sort(function (a, b) {
+				return b.punti - a.punti;
+			})
+
+			for (var i = 0; i < game.length; i++) {
+				if (i == 0) {
+					playerScore(100, game[i]['id']);
+				} else {
+					playerScore(game[0]['punti'], game[i]['id']);
 				}
 			}
-			for (var j = 1; j <= $scope.tavoli; j++) {
-				
-			}
-			
-			for (var i = 0; i <= game.length; i++) {
-				if (game[i]["tavolo"] == i) {
-					table.push(game[i])
-				}
-				table = table.sort( function (a,b) {
-					return b.punti - a.punti;
-				});
-				for (var j = 0; j < table.length; j++) {
-					if (i == 0) { 
-						table[0]["score"] = 100;
+		}
+
+		var playerScore = function (hScore, id) {
+			for (var j = 0; j < $scope.data.partite.length; j++) {
+				if ($scope.data.partite[j]['id'] == id) {
+					if (hScore == 100) {
+						$scope.data.partite[j]['score'] = '100';
 					} else {
-						table[i]["score"] = Math.floor(table[i]["punti"]/table[0]["punti"]);
+						var scoreValue = Math.floor($scope.data.partite[j]['punti']/hScore * 100);
+						$scope.data.partite[j]['score'] = scoreValue;
 					}
 				}
 			}
--- Torneo/controllers/torneoController.js
+++ Torneo/controllers/torneoController.js
@@ -2,21 +2,21 @@ angular.module("torneo")
 	.controller("torneoCtrl", function ($scope) {
 
     $scope.data = {
-    	partito: [
-        { partita: 1, tavolo: 1, giocatore: "Matteo", punti: 23, conclusa: false },
-		{ partita: 1, tavolo: 1, giocatore: "Ivo", punti: 34, conclusa: false },
-		{ partita: 1, tavolo: 1, giocatore: "Ludo", punti: 18, conclusa: false },
-		{ partita: 1, tavolo: 1, giocatore: "Beppe", punti: 9, conclusa: false },
-		{ partita: 1, tavolo: 2, giocatore: "Matteo", punti: 56, conclusa: false },
-		{ partita: 1, tavolo: 2, giocatore: "Ivo", punti: 32, conclusa: false },
-		{ partita: 1, tavolo: 2, giocatore: "Ludo", punti: 1, conclusa: false },
-		{ partita: 1, tavolo: 2, giocatore: "Beppe", punti: 17, conclusa: true },
-		{ partita: 2, tavolo: 2, giocatore: "Matteo", punti: 6, conclusa: false },
-		{ partita: 2, tavolo: 2, giocatore: "Ivo", punti: 22, conclusa: true},
-		{ partita: 2, tavolo: 2, giocatore: "Ludo", punti: 71, conclusa: false},
-		{ partita: 2, tavolo: 2, giocatore: "Beppe", punti: 57, conclusa: true}],
+    	partite: [
+        { partita: 1, tavolo: 1, giocatore: "Matteo", punti: 23, conclusa: false, score: 0, id: 1 },
+		{ partita: 1, tavolo: 1, giocatore: "Ivo", punti: 34, conclusa: false, score: 0, id: 2 },
+		{ partita: 1, tavolo: 1, giocatore: "Ludo", punti: 18, conclusa: false, score: 0, id: 3 },
+		{ partita: 1, tavolo: 1, giocatore: "Beppe", punti: 9, conclusa: false, score: 0, id: 4},
+		{ partita: 1, tavolo: 2, giocatore: "Matteo", punti: 56, conclusa: false, score: 0, id: 5 },
+		{ partita: 1, tavolo: 2, giocatore: "Ivo", punti: 32, conclusa: false, score: 0, id: 6 },
+		{ partita: 1, tavolo: 2, giocatore: "Ludo", punti: 1, conclusa: false, score: 0, id: 7 },
+		{ partita: 1, tavolo: 2, giocatore: "Beppe", punti: 17, conclusa: true, score: 0, id: 8 },
+		{ partita: 2, tavolo: 2, giocatore: "Matteo", punti: 6, conclusa: false, score: 0, id: 9 },
+		{ partita: 2, tavolo: 2, giocatore: "Ivo", punti: 22, conclusa: true, score: 0, id: 10},
+		{ partita: 2, tavolo: 2, giocatore: "Ludo", punti: 71, conclusa: false, score: 0, id: 11},
+		{ partita: 2, tavolo: 2, giocatore: "Beppe", punti: 57, conclusa: true, score: 0, id: 12}],
 
-		partite: [],
+		partito: [],
 
 		classifica: [
 		{ giocatore: "Matteo", punteggio: 0},
@@ -30,7 +30,7 @@ angular.module("torneo")
 
 		nome: "",
 
-		activeGame: 1 
+		activeGame: 2 
 	};
 
 	function HeaderController($scope, $location) 
@@ -44,8 +44,8 @@ angular.module("torneo")
 		return b.punteggio - a.punteggio;
 	});
 
-	$scope.sorted = $scope.data.partite.filter( function (obj) {
-		return obj.tavolo == 1;
-	}); 
+	$scope.sorted = $scope.data.partite.filter(function (obj) {
+		return obj.partita == $scope.data.activeGame;
+	});
 
 });
